b9c2b9c5cea42cedce687fa2d0549c09c2a3e803
Ярыгин Максим Олегович <ip916s28@mwp03.csc.local>
2020-02-19 12:14:20 +0700

    reate main.c with board

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <.stdio.h>
+int main()
+{
+    int i, j;
+    printf("chess board:\n");
+    for (i = 0; i <= 7; i++) {
+        for (j = 0; j <= 7; j++) {
+            if (i % 2 == 1)
+                if (j % 2 == 1)
+                    printf("  ");
+                else
+                    printf("*");
+            else if (j % 2 == 1)
+                printf("*");
+            else
+                printf("  ");
+        }
+    }
+}
+
